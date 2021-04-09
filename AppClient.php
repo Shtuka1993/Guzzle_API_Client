150ea16bddfe7ba7344d563aa4fd8f443c25ca7c
yshtokaylo <yshtokaylo@artelogic.net>
2021-04-10 00:17:35 +0300

    Removed unneeded comments.

--- AppClient.php
+++ AppClient.php
@@ -15,9 +15,7 @@
 
         public function __construct() {
             $this->client = new Client([
-                // Base URI is used with relative requests
                 'base_uri' => self::API_URL,
-                // You can set any number of default request options.
                 'timeout'  => 2.0,
             ]);
         }
@@ -47,7 +45,6 @@
             //$parsed = Request::parseHeader($response);
             $data = json_decode((string)$body); 
         
-            //Code for Logger
             $clientLogger->loggRequest($request);
             $clientLogger->loggResponse($body);
         
