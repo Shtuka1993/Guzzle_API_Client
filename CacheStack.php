f538f0dad33fd85487f7df557ff4a96d04a6e1c0
yshtokaylo <yshtokaylo@artelogic.net>
2021-04-13 01:00:19 +0300

    Changing of cache class

--- CacheStack.php
+++ CacheStack.php
@@ -1,24 +1,40 @@
 <?php
     use GuzzleHttp\HandlerStack;
-    use League\Flysystem\Filesystem;
-    use League\Flysystem\Local\LocalFilesystemAdapter;
+    /*use League\Flysystem\Filesystem;
+    use League\Flysystem\Adapter\Local;*/
+    use Doctrine\Common\Cache\FilesystemCache;
     use Kevinrob\GuzzleCache\CacheMiddleware;
     use Kevinrob\GuzzleCache\Strategy\PrivateCacheStrategy;
     use Kevinrob\GuzzleCache\Storage\FlysystemStorage;
  
     class CacheStack {
+        const CACHE_STORAGE = "/local/cache";
         private $stack;
 
         public function __construct() {
             $this->$stack = HandlerStack::create();
-            $adapter = new LocalFilesystemAdapter("/local/cache");
+            /*$adapter = new Local("/local/cache");
             $this->$stack->push(
                 new CacheMiddleware(
                     new PrivateCacheStrategy(
-                        $adapter
+                        new FlysystemStorage($adapter)
                     )
                 ), 
                 "cache"
-            );
+            );*/
+            $this->stack->push(
+                new CacheMiddleware(
+                  new PrivateCacheStrategy(
+                    new DoctrineCacheStorage(
+                      new FilesystemCache(self::CACHE_STORAGE)
+                    )
+                  )
+                ),
+                'cache'
+              );
+        }
+
+        public function getStack() {
+            return $this->stack;
         }
     }
